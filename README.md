61b210d94c48b46df0615d582d69a64e7c8fd4e1
Bamidelejulius12 <babatundejulius911@gmail.com>
2026-02-28 14:42:44 +0100

    updated project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vehicle_insurance
+this project is used for practice of end to end mlops project pipeline
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author = "Bamidele Julius",
+    author_email="babatundejulius911@gmail.com",
+    packages= find_packages()
+)
